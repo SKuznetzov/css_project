a18fe6e2cc5469bff173b83b68a4c16c7c9d29b0
SKuznetzov <s-kuznecov100@rambler.ru>
2021-10-22 16:46:39 +0300

    main.js

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+const mixer = mixitup ('.container-filter');
+
+$(document).ready(function () {
+    $('a[href^="#"]').click(function () {
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop:$(target).offset().top
+        },500);
+        return false;
+    });
+});
